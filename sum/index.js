96d5e37a4a87f0b0e21f79b80c934b27bbbd4b67
YamaTatsu10969 <52.wadachi.96@gmail.com>
2021-04-30 16:40:25 +0900

    add sum package

---
+++ sum/index.js
@@ -0,0 +1,23 @@
+/**
+ * 数値の配列を受け取って、その要素の合計を返す関数
+ */
+function add(numbers) {
+  let result = 0;
+  for (const num of numbers) {
+    result = result + num;
+  }
+  return result;
+}
+
+/**
+ * 数値の配列を受け取って、その要素を全て掛けた数字を返す関数
+ */
+function multi(numbers) {
+  let result = 1;
+  for (const num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+module.exports = { add, multi };
